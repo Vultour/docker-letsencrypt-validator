dad0d1d769c5e61aeb2dabb5f0bf98bcaa48b8fa
Martin Kukura <git@mxt.sk>
2018-07-15 01:30:51 +0100

    Add combined directory for HAProxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,12 @@
 
 . /srv/variables.sh
 
+mkdir -p /etc/letsencrypt/combined
+
 while true; do
         for domain in $DOMAINS; do
                 certbot certonly --webroot --webroot-path /mnt/letsencrypt/ -n --agree-tos -m $EMAIL -d $domain
+                cat /etc/letsencrypt/live/$domain/{fullchain.pem,privkey.pem} > /etc/letsencrypt/combined/${domain}.pem
         done
         sleep $(python2 -c "import random;t = random.randint(0, 3600);print (t + 43200)")
 done
