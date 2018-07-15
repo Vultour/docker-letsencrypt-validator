caa8ae473c09e8c772d7cd154e2b2696b5d39f7d
Martin Kukura <git@mxt.sk>
2018-07-15 01:41:30 +0100

    Expand expression for non-bash support

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /etc/letsencrypt/combined
 while true; do
         for domain in $DOMAINS; do
                 certbot certonly --webroot --webroot-path /mnt/letsencrypt/ -n --agree-tos -m $EMAIL -d $domain
-                cat /etc/letsencrypt/live/$domain/{fullchain.pem,privkey.pem} > /etc/letsencrypt/combined/${domain}.pem
+                cat /etc/letsencrypt/live/$domain/fullchain.pem /etc/letsencrypt/live/$domain/privkey.pem > /etc/letsencrypt/combined/${domain}.pem
         done
         sleep $(python2 -c "import random;t = random.randint(0, 3600);print (t + 43200)")
 done
